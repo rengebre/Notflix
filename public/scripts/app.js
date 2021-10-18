af4b8075fb34b516263a498de2c1d24f6d5aa6be
panoramine <camila.c.a.salles@gmail.com>
2021-10-18 08:30:30 -0700

    add functionality to cancel button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,16 @@ $(document).ready(function() {
   $(".forms-container").hide();
 
   $(".button").on("click", function() {
-
     $(".button").hide();
     $(".description").hide();
 
     $(".forms-container").show();
+  })
+
+  $(".cancel-button").on("click", function() {
+    $(".forms-container").hide();
+
+    $(".button").show();
+    $(".description").show();
   })
 });
