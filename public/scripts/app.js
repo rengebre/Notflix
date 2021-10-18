9049eed2490baa743c0b0c48d855218172b799e2
panoramine <camila.c.a.salles@gmail.com>
2021-10-18 08:09:10 -0700

    html for forms-container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+  $(".forms-container").hide();
+
+  $(".button").on("click", function() {
+
+    $(".button").hide();
+    $(".description").hide();
+
+    $(".forms-container").show();
+  })
+});
