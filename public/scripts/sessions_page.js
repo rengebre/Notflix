90be018567dc080c8c2550df81c39e6e59602632
Russell Engebretson <russell.engebretson@gmail.com>
2021-10-22 06:53:26 +0000

    fix issue with final gif

--- public/scripts/sessions_page.js
+++ public/scripts/sessions_page.js
@@ -63,6 +63,7 @@ $(document).ready(function() {
         $('#poster-overlay').remove();
         $moviePosterDiv.addClass('final-gif');
         $moviePosterDiv.removeAttr('id');
+        $('section.session-poster-display').css('max-width', '1000px')
 
         return;
       }
